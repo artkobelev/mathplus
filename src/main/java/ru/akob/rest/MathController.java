d0e492bfac23ab7e11530498bc7e2a9edc639dac
akobelev <akobelev@naumen.ru>
2019-05-08 15:04:28 +0300

    init

---
+++ src/main/java/ru/akob/rest/MathController.java
@@ -0,0 +1,14 @@
+package ru.akob.rest;
+
+import java.util.List;
+
+public interface MathController
+{
+    Double average(List<Double> numbers);
+
+    Long dec(Long number);
+
+    Long inc(Long number);
+
+    Double sum(List<Double> numbers);
+}
---
+++ src/main/java/ru/akob/rest/MathControllerImpl.java
@@ -0,0 +1,44 @@
+package ru.akob.rest;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/math")
+public class MathControllerImpl implements MathController
+{
+    @RequestMapping(value = "avg", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @Override public Double average(@RequestParam(name = "numbers") List<Double> numbers)
+    {
+        return numbers.stream().mapToDouble(val -> val).average().orElse(0.0);
+    }
+
+    @RequestMapping(value = "dec/{number}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @Override public Long dec(@PathVariable(name = "number") Long number)
+    {
+        return --number;
+    }
+
+    @RequestMapping(value = "/inc/{number}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @Override public Long inc(@PathVariable(name = "number") Long number)
+    {
+        return ++number;
+    }
+
+    @RequestMapping(value = "/sum", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @Override public Double sum(@RequestParam(name = "numbers") List<Double> numbers)
+    {
+        return numbers.stream().mapToDouble(i -> i).sum();
+    }
+}
